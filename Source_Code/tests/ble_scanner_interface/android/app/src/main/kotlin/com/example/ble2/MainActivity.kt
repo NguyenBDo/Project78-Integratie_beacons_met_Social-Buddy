c1b73d5a45188148dffe20c9a255ad1c956d94f0
NguyenBDo <135064659+NguyenBDo@users.noreply.github.com>
2024-06-04 14:56:01 +0200

    remove large file

--- Source_Code/tests/ble_scanner_interface/android/app/src/main/kotlin/com/example/ble2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ble2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Source_Code/tests/ble_scanner_interface/android/app/src/main/kotlin/com/example/ble_scanner_interface/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ble_scanner_interface
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
